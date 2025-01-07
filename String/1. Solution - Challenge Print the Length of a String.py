ab9986d8c49eaf5c225a6f65a800f532d349028e
nareshkumar0610 <nareshkumar061097.nk@gmail.com>
2025-01-08 00:38:28 +0530

    Add files via upload

---
+++ String/1. Solution - Challenge Print the Length of a String.py
@@ -0,0 +1,19 @@
+# Method 1:
+S = input("Type the word to find the lenght of the word: ")
+print(len(S))
+
+
+# Method 2:
+s = input("Type the word to find the lenght of the word: ")
+count = int()
+
+for i in s:
+    count+=1
+print(count)
+
+
+
+
+
+
+
