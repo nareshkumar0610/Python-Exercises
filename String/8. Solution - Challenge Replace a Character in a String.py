233fdddad0863606ba2fc079687b2cca804fccad
nareshkumar0610 <nareshkumar061097.nk@gmail.com>
2025-01-08 00:21:53 +0530

    Delete String directory

--- String/8. Solution - Challenge Replace a Character in a String.py
+++
@@ -1,9 +0,0 @@
-
-s = input("Enter anything: ")
-curr_char = input("Enter the character which you want to replace: ")
-new_Char = input("Enter the new character you want to add: ")
-
-new_word = s.replace(curr_char, new_Char)
-print(f"You replaced the character '{curr_char}' with '{new_Char}' and the new word is '{new_word}'.")
-
-
